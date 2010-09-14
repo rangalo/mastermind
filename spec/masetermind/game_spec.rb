6df2761bb131cb5a0b6a8ae74018d08d71b86448
hardik <rangalo.bhavai@gmail.com>
2010-09-14 17:33:03 +0200

    first feature complete with red, green, refactor

--- spec/masetermind/game_spec.rb
+++ spec/masetermind/game_spec.rb
@@ -2,5 +2,20 @@ require File.join(File.dirname(__FILE__), "/../spec_helper" )
 
 module Mastermind
     describe Game do
+        before(:each) do
+            @messenger = mock("messenger").as_null_object
+            @game = Game.new(@messenger)
+        end
+        context "starting up" do
+            it "should send a welcome message" do
+                @messenger.should_receive(:puts).with("Welcome to Mastermind!")
+                @game.start
+            end
+
+            it "should prompt for the first guess" do
+                @messenger.should_receive(:puts).with("Enter guess:")
+                @game.start
+            end
+        end
     end
 end
